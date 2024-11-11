4942be7f23e0388651d5decdf1723a80e5a85247
lucasbouet30 <153817043+lucasbouet30@users.noreply.github.com>
2024-11-11 16:57:13 +0100

    Update app.py

--- V0.1.0/app.py
+++ V0.1.0/app.py
@@ -205,18 +205,6 @@ def support():
 @app.route('/profile')
 def profile():
     return render_template('profile.html')
-
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
-@app.route('/register')
-def register():
-    return render_template('register.html')
-
-@app.route('/logout')
-def logout():
-    return render_template('logout.html')
     
 # END rendering webpages
 
